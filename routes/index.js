ae8a65d8311a76fae8ad045c940d6ae3888c33b9
Jamila Cocchiola <jamila.khawaja85@gmail.com>
2023-03-06 20:19:23 -0500

    update

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const indexRouter = express.Router()
+const path = require('path')
+
+indexRouter.get('/', (req, res) => {
+    res.sendFile(path.resolve('./index.html'))
+})
+
+indexRouter.all('*', (req, res) => {
+    res.status(404).sendFile(path.resolve(__dirname, '../public/pages/404.html'))
+  })
+
+module.exports = indexRouter
